16b1430eec9ba94ed9f431458605b6c455e40ef3
Nhii0492 <116798212+Nhii0492@users.noreply.github.com>
2024-10-29 15:00:51 +0700

    [Nhi] online consultant

--- MedicalClinic/lib/online_consultant.dart
+++ MedicalClinic/lib/online_consultant.dart
@@ -27,6 +27,7 @@ class OnlineConsultationScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: Colors.white,
       appBar: AppBar(
         backgroundColor: Colors.white,
         elevation: 0,
