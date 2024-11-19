d2659d88672999be727c62c7087581b6a6379888
dominhhuy1703 <dominhhuy1703@gmail.com>
2024-11-19 13:37:18 +0700

    [HUY] Update new UI

--- MedicalClinic/lib/main.dart
+++ MedicalClinic/lib/main.dart
@@ -14,7 +14,7 @@ class MyApp extends StatelessWidget {
     return ChangeNotifierProvider(
       create: (context) => TokenProvider(),
       child: MaterialApp(
-        home: LoginScreen(),
+        home: WelcomeScreen(),
         debugShowCheckedModeBanner: false,
       ),
     );
